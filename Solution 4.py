c8727ecb35ee4ac5cea2f42ed73aec9b1ea47787
Valentino <valentinobenedetich@gmail.com>
2023-01-09 23:04:32 -0300

    Solucion 4

---
+++ Solution 4.py
@@ -0,0 +1,25 @@
+e1 = "aaabaaa"
+e2 = "mama mia en la pizzeria amam"
+e3 = "a"
+e4 = "bbdaadbb"
+e5 = "lorem ipsum dolor sit amettema tis rolod muspi merol"
+def checkMirror(x):
+
+    l = len(x) - 1
+    
+    i = 0
+    while ( x[i]==x[l-i] ) and ( i < l - i ):
+        i += 1
+
+    if i >= l-i:
+        return True
+    else:
+        return False
+    
+
+# No Tocar
+print(checkMirror(e1))
+print(checkMirror(e2))
+print(checkMirror(e3))
+print(checkMirror(e4))
+print(checkMirror(e5))
